3fa94e6a29fe94e94876a4ac4e3124433857ea21
cesarglez133 <121037947@upq.edu.mx>
2023-02-24 16:14:29 -0600

    Agregamos los getters y setters

--- Personaje.py
+++ Personaje.py
@@ -1,9 +1,9 @@
 class Personaje: 
     #Definimos el constructor de personaje
     def __init__(self,esp,nom,alt):
-        self.especie = esp
-        self.nombre = nom
-        self.altura = alt 
+        self.__especie = esp
+        self.__nombre = nom
+        self.__altura = alt 
     
     #atributos Personaje
     especie = "Humano"
@@ -13,14 +13,32 @@ class Personaje:
     #metodos Personaje
     def correr(self, status):
         if (status):
-            print("El personaje " +self.nombre+ " esta corriendo")
+            print("El personaje " +self.__nombre+ " esta corriendo")
         else:
-            print("El personaje " +self.nombre+ " se detuvo")
+            print("El personaje " +self.__nombre+ " se detuvo")
         
     def lanzarGranadas(self):
-        print("El personaje " +self.nombre+ " lanzo una granada")
+        print("El personaje " +self.__nombre+ " lanzo una granada")
         
     def recargar(self, municiones):
         cargador= 10
         cargador= cargador + municiones
         print ("El arma tiene " + str(cargador) + " balas")
+        
+    #Declaramos los metodos get y set
+    def getNombre(self):
+        return self.__nombre
+    def setNombre(self,nom):
+        self.__nombre = nom
+        
+    def getEspecie(self):
+        return self.__especie
+    def setEspecie (self,esp):
+        self.__especie = esp
+        
+    def getAltura(self):
+        return self.__altura
+    def setAltura (self, alt):
+        self.__altura = alt
+        
+    
